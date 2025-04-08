563907ed1780bfa94d1d7ee0b8649a04fb1e2c9d
ARTMashimbyi <133565194+ARTMashimbyi@users.noreply.github.com>
2025-04-08 20:27:29 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,1 @@
-// db.js
-const sql = require('mssql');
 
-const config = {
-  user: 'constitutionvault',
-  password: 'Code@mash',
-  server: 'constitutionvault.database.windows.net',
-  database: 'constitutionvault',
-  options: {
-    encrypt: true,
-    trustServerCertificate: false
-  }
-};
-
-module.exports = {
-  sql,
-  config
-};
